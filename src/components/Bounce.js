17b251011a0215a9b7b2989f900ca6316fc9651a
Holyweb232 <vadimkohan2@gmail.com>
2019-10-20 16:04:20 +0300

    add Preloader 20.10

--- src/components/Bounce.js
+++ src/components/Bounce.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import styled, {keyframes} from 'styled-components';
 import {bounce} from 'react-animations';
-
+import ball from "../assets/images/ball.png";
 
 const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
 export default class ReactAnimations extends Component {
     render() {
         return (
-            <Bounce><img src='https://igreen.in.ua/images/frenchDog.png' alt="frenchDog"/></Bounce>
+            <Bounce><img src={ball} alt="ball"/></Bounce>
         );
     }
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,41 +1,29 @@
 import React from 'react'
-import styles from './style.module.css'
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/user.png'
+import styles from "./style.module.css";
+import userPhoto from "../../assets/images/user.png";
 
+let Users = (props) => {
 
-class Users extends React.Component {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
-    }
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++){
-            pages.push(i);
-        }
-        return <div>
-            <div>
-                {pages.map(p => {
-                   return <span key={p.id} className={this.props.currentPage === p && styles.selectedPage}
-                   onClick={(e) => {this.onPageChanged(p)}}>{p}</span>
-                })}
+    return <div>
 
-            </div>
-            {
-                this.props.users.map(u =>
-                    <div key={u.id}>
+        <div>
+            {pages.map(p => {
+                return <span key={p.id} className={props.currentPage === p && styles.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
+            })}
+
+        </div>
+        {
+            props.users.map(u =>
+                <div key={u.id}>
                         <span>
                             <div>
                                 <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar"
@@ -44,14 +32,14 @@ class Users extends React.Component {
                             <div>
                                 {u.followed
                                     ? <button onClick={() => {
-                                        this.props.unfollow(u.id)
+                                        props.unfollow(u.id)
                                     }}>Unfollowed</button>
                                     : <button onClick={() => {
-                                        this.props.follow(u.id)
+                                        props.follow(u.id)
                                     }}>Followed</button>}
                             </div>
                         </span>
-                        <span>
+                    <span>
                             <span>
                                 <div>{u.name}</div>
                                 <div>{u.status}</div>
@@ -61,11 +49,9 @@ class Users extends React.Component {
                                 <div>{"u.location.country"}</div>
                             </span>
                         </span>
-                    </div>)
-            }
-        </div>
-
-    }
+                </div>)
+        }
+    </div>
 }
 
 export default Users
